ad9eec4643b7632025196f993e383947208eace4
Zafar Bekliev <inspiring.web@yandex.ru>
2019-01-03 20:52:04 +0300

    Educating...

--- index.js
+++ index.js
@@ -2,6 +2,43 @@ var app = new Vue({
   el: "#app",
   data: {
     product: "Socks",
-    image: "./vmSocks-green.jpg"
+    image: "vmSocks-green.jpg",
+    alttext: "Green sock",
+    title: "Green sock",
+    inventory: 100,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green",
+        variantImage: "vmSocks-green.jpg"
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue",
+        variantImage: "vmSocks-blue.jpg"
+      }
+    ],
+    sizesArr: ["X", "XL", "XXL"],
+    zafarqwe: ["Zafarchik", "Zafarjon", "Zafarbek", "Zappishka"],
+    person: {
+      firstName: "Zafar",
+      lastName: "Bekliev",
+      age: 26
+    },
+    cart: 0,
+    minus1: 100
+  },
+
+  methods: {
+    addtocart: function() {
+      this.cart += 1;
+    },
+    updateProduct: function(variantImage) {
+      this.image = variantImage;
+    },
+    minus: function() {
+      this.minus1 -= 1;
+    }
   }
 });
